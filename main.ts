9e2364ab7f892ef0157a3c053f6e741c02861bd5
tyamada99 <tyamada99@gmail.com>
2026-06-09 18:06:30 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,19 +7,21 @@ let 経過時間 = 0
 let スタート時刻 = 0
 let runnning = 0
 runnning = 0
+// センサーが安定するまで待機
+basic.pause(1000)
 basic.forever(function () {
-    if (pins.analogReadPin(AnalogPin.P0) < 500) {
+    if (pins.analogReadPin(AnalogPin.P0) > 500) {
         if (runnning == 0) {
             スタート時刻 = control.millis()
             runnning = 1
             basic.showString("S")
-            basic.pause(500)
+            // チャタリング防止
+            basic.pause(100)
         } else {
             経過時間 = control.millis() - スタート時刻
             タイム = Math.round(経過時間 / 10) / 100
             basic.clearScreen()
             basic.showNumber(タイム)
-            basic.pause(1000)
             runnning = 0
         }
     }
